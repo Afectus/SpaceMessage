f38c0839de39280970ce7519ad23ea673d0cb51d
Afectus <gren911@yandex.ru>
2018-01-25 17:51:17 +0700

    Добавил для поля с токеном свойство уникальности

--- Rootspace/space/apps/backend/models.py
+++ Rootspace/space/apps/backend/models.py
@@ -11,15 +11,35 @@ class SpaceMessage(models.Model):
     date = models.DateField(auto_now_add=True)
     status = models.BooleanField(default=False)
 
+#stringForToken = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+#52^4= 7311616 возможных токенов
+    #while token == '' or token == TokenGenerator.objects.filter(tokens=token):
+#def maketokem():
+#    stringForToken = 'qw'
+#    token = ''
+#    while True:
+#
+#        if token == '' or token == TokenGenerator.objects.filter(tokens=token):
+#            for x in range(1):
+#                s = random.choice(stringForToken)
+#                token = token + s
+#                kek = TokenGenerator.objects.filter(tokens=token)
+#                # print(TokenGenerator.objects.filter(tokens=token))
+#                print('-' * 50)
+#                print(kek)
+#                print(s)
+#                break
+#        else:
+#            return token
 
 def maketokem():
-    stringForToken = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    stringForToken = 'qw'
     token = ''
-    for x in range(4):
+    for x in range(1):
         s = random.choice(stringForToken)
         token = token + s
+
     return token
-    
 class TokenGenerator(models.Model):
-    tokens = models.CharField(default=maketokem, max_length=255)
+    tokens = models.CharField(default=maketokem, max_length=255, unique=True)
     status = models.BooleanField(default=False)
